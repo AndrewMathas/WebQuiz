a3c57a9833eb9de48ee2956c0093f703418233ad
Andrew Mathas <andrew.mathas@gmail.com>
2019-01-28 20:18:11 +1100

    More documentation, changuing response to feedback, ...

--- webquiz/webquiz_xml.py
+++ webquiz/webquiz_xml.py
@@ -17,7 +17,6 @@ r'''
 
 # -*- encoding: utf-8 -*-
 
-import sys
 import xml.sax
 
 # imports of webquiz code
@@ -66,7 +65,7 @@ class QuizHandler(xml.sax.ContentHandler):
         initialises the webquiz tags, and then `endElement` attaches the
         content of each webquiz tag tothe appropriate part of `self`. Any end
         tag that is ot special to webquiz has its contents appended to
-        `self.text`. Any tag that contains `deFAULT` is set to the system
+        `self.text`. Any tag that contains `DeFaUlT` is set to the system
         default using the `defaults` dictionary.
     """
 
@@ -84,7 +83,7 @@ class QuizHandler(xml.sax.ContentHandler):
         self.meta_list = []
 
         # to add mathjs when an eval comparison is used
-        self.mathjs = False  
+        self.mathjs = False
 
         # the following tags have defaults set by `defaults`
         self.setting_tags = [
@@ -111,14 +110,15 @@ class QuizHandler(xml.sax.ContentHandler):
 
     def set_default_attribute(self, key, value):
         ''' Set the attribute `key` of self, using the default value if
-        `value=='deFAULT'`.
+        `value=='DeFaUlT'`.
         '''
-        if value.strip() == 'deFAULT':
+        if value.strip() == 'DeFaUlT':
             setattr(self, key, self.defaults[key])
         else:
             setattr(self, key, value)
         debugging('Just set "{}" equal to "{}" from "{}"'.format(key, getattr(self, key), value))
 
+    #---- start of start elements --------------------------------------------
     def startElement(self, tag, attributes):
         '''
             At the start of each webquiz xml tag we need to pull out the
@@ -127,158 +127,163 @@ class QuizHandler(xml.sax.ContentHandler):
 
         self.current_tags.append(tag)
 
-        # initialise the quiz
-        if tag == 'webquiz':
-            for key in attributes.keys():
-                self.set_default_attribute(key, attributes.get(key))
-
-            # convert the following attibutes to booleans
-            for key in ['debugging', 'hide_side_menu', 'one_page', 'pst2pdf', 'random_order']:
-                setattr(self, key, getattr(self, key)=='true')
-
-            # set debugging mode from the latex file...from this point on
-            metadata.debugging = metadata.debugging or self.debugging
-
-        # set up links, meta tags and department and unit data
-        elif tag == 'link':
-            self.link_list.append({key: attributes.get(key) for key in attributes.keys()})
-
-        elif tag == 'meta':
-            self.meta_list.append({key: attributes.get(key) for key in attributes.keys()})
+        if hasattr(self, 'start_'+tag):
+            getattr(self, 'start_'+tag)(attributes)
 
         elif tag in ['department', 'institution', 'uni']:
             for key in attributes.keys():
                 self.set_default_attribute(tag, attributes.get(key))
 
-        elif tag == 'breadcrumb':
-            self.set_default_attribute('breadcrumbs', attributes.get('breadcrumbs'))
+    def start_webquiz(self, attributes):
+        r'''
+        Start element for tag="webquiz". Initialise the quix and extract
+        and process the list of attributes.
+        '''
+        for key in attributes.keys():
+            self.set_default_attribute(key, attributes.get(key))
 
-        elif tag == 'unit_name':
-            self.set_default_attribute('unit_url', attributes.get('url'))
-            self.quizzes_url = attributes.get('quizzes_url')
-            if self.quizzes_url == 'deFAULT':
-                self.quizzes_url = self.unit_url + '/Quizzes'
+        # convert the following attibutes to booleans
+        for key in ['debugging', 'hide_side_menu', 'one_page', 'pst2pdf', 'random_order']:
+            setattr(self, key, getattr(self, key)=='true')
 
-        # set up questions and discussion
-        elif tag == 'discussion':
-            discussion = Data(heading = '',
-                              short_heading = '',
-                              text = ''  # The text of the discussion
+        # set debugging mode from the latex file...from this point on
+        metadata.debugging = metadata.debugging or self.debugging
+
+
+    def start_link(self, attributes):
+        r'''
+        Start element for tag="link"
+        '''
+        self.link_list.append({key: attributes.get(key) for key in attributes.keys()})
+
+    def start_meta(self, attributes):
+        r'''
+        Start element for tag="meta"
+        '''
+        self.meta_list.append({key: attributes.get(key) for key in attributes.keys()})
+
+    def start_breadcrumb(self, attributes):
+        r'''
+        Start element for tag="breadcrumb"
+        '''
+        self.set_default_attribute('breadcrumbs', attributes.get('breadcrumbs'))
+
+    def start_unit_name(self, attributes):
+        r'''
+        Start element for tag="unit_name"
+        '''
+        self.set_default_attribute('unit_url', attributes.get('url'))
+        self.quizzes_url = attributes.get('quizzes_url')
+        if self.quizzes_url == 'DeFaUlT':
+            self.quizzes_url = self.unit_url + '/Quizzes'
+
+    def start_discussion(self, attributes):
+        r'''
+        Start element for tag="discussion"
+        '''
+        discussion = Data(heading = '',
+                          short_heading = '',
+                          text = ''  # The text of the discussion
+        )
+        self.discussion_list.append(discussion)
+
+    def start_question(self, attributes):
+        r'''
+        Start element for tag="question"
+        '''
+        self.question_list.append(
+            Data(text = '',      # The text of the question
+                type = None,    # input, or single or multiple choice
+                after_text = '' # text at end of question
             )
-            self.discussion_list.append(discussion)
+        )
 
-        elif tag == 'question':
-            self.question_list.append(
-                Data(text = '',      # The text of the question
-                    type = None,    # input, or single or multiple choice
-                    after_text = '' # text at end of question
-                )
+    def start_answer(self, attributes):
+        r'''
+        Process the different question types, items choice and feedback
+        '''
+        if self.question_list[-1].type != None:
+            webquiz_error('question {} has too many question types: {} and input'.format(
+                    len(self.question_list)+1, self.question_list[-1].type)
             )
+        self.question_list[-1].type = 'input'
+        self.question_list[-1].answer = ''
+        self.question_list[-1].feedback_right = ''
+        self.question_list[-1].feedback_wrong = ''
+        self.question_list[-1].text += self.text
+        self.text = ''
 
-        # process the different question types, items choice and responses
-        elif tag == 'answer':
-            if self.question_list[-1].type != None:
-                webquiz_error('question {} has too many question types: {} and input'.format(
-                        len(self.question_list)+1, self.question_list[-1].type)
-                )
-            self.question_list[-1].type = 'input'
-            self.question_list[-1].answer = ''
-            self.question_list[-1].when_right = ''
-            self.question_list[-1].when_wrong = ''
-            self.question_list[-1].text += self.text
+        self.question_list[-1].comparison = attributes.get('comparison')
+        self.question_list[-1].prompt = attributes.get('prompt')=='true'
+        if self.question_list[-1].comparison in ['complex', 'number']:
+            self.mathjs = True
+
+    def start_choice(self, attributes):
+        r'''
+        Start element for tag="choice"
+        '''
+        if self.question_list[-1].type != None:
+            webquiz_error('question {} has too many question types: {} and choice'.format(
+                    len(self.question_list)+1, self.question_list[-1].type)
+            )
+        self.question_list[-1].type = attributes.get('type')
+        self.question_list[-1].columns = int(attributes.get('columns'))
+        self.question_list[-1].items = []
+        self.question_list[-1].correct = 0
+        self.question_list[-1].text += self.text
+        self.text = ''
+
+    def start_item(self, attributes):
+        r'''
+        Start element for tag="item"
+        '''
+        self.question_list[-1].items.append(
+                Data(correct= attributes.get('correct'),
+                     symbol=attributes.get('symbol'),
+                     feedback='',
+                     text=''
+                    )
+        )
+        if attributes.get('correct')=='true':
+            self.question_list[-1].correct += 1
+
+    def start_index_item(self, attributes):
+        r'''
+        Finally look after the index file
+        '''
+        self.quiz_index.append(Data(
+                prompt=attributes.get('prompt')=='true',
+                url=attributes.get('url'),
+                title=''
+            )
+        )
+
+    def start_when(self, attributes):
+        r'''
+        start element for tag="when"
+        '''
+        if self.text.strip() != '':
+            self.question_list[-1].after_text += ' '+self.text.strip()
+            debugging('After_text is now {}'.format(self.question_list[-1].after_text))
             self.text = ''
+        self.current_tags[-1] = 'feedback_'+attributes.get('type')
 
-            self.question_list[-1].comparison = attributes.get('comparison')
-            self.question_list[-1].prompt = attributes.get('prompt')=='true'
-            if self.question_list[-1].comparison in ['complex', 'number']:
-                self.mathjs = True
-
-        elif tag == 'choice':
-            if self.question_list[-1].type != None:
-                webquiz_error('question {} has too many question types: {} and choice'.format(
-                        len(self.question_list)+1, self.question_list[-1].type)
-                )
-            self.question_list[-1].type = attributes.get('type')
-            self.question_list[-1].columns = int(attributes.get('columns'))
-            self.question_list[-1].items = []
-            self.question_list[-1].correct = 0
-            self.question_list[-1].text += self.text
-            self.text = ''
-
-        elif tag == 'item':
-            self.question_list[-1].items.append(
-                    Data(correct= attributes.get('correct'),
-                         symbol=attributes.get('symbol'),
-                         response='',
-                         text=''
-                        )
-            )
-            if attributes.get('correct')=='true':
-                self.question_list[-1].correct += 1
-
-        # finally look after the index file
-        elif tag == 'index_item':
-            self.quiz_index.append(Data(
-                    prompt=attributes.get('prompt')=='true',
-                    url=attributes.get('url'),
-                    title=''
-                )
-            )
-
-        elif tag == 'when':
-            if self.text.strip() != '':
-                self.question_list[-1].after_text += ' '+self.text.strip()
-                debugging('After_text is now {}'.format(self.question_list[-1].after_text))
-                self.text = ''
-            self.current_tags[-1] = 'when_'+attributes.get('type')
-
+    #---- end of start elements ---------------------------------------------
 
     def endElement(self, tag):
         debugging('popping '+self.current_tags.pop()) # remove the last tag from the tag list
 
-        text_used = True # assume that we will use the text
+        reset_text = True
+        if hasattr(self, 'end_'+tag):
+            getattr(self, 'end_'+tag)()
+            self.text = ''
 
-        if tag in self.setting_tags:
+        elif tag in self.setting_tags:
             self.set_default_attribute(tag, self.text)
-
-        elif tag == 'answer':
-            self.question_list[-1].answer = self.text.strip()
-
-        elif tag == 'discussion':
-            self.discussion_list[-1].text = self.text.strip()
+            self.text = ''
 
         elif tag in ['heading', 'short_heading']:
             setattr(self.discussion_list[-1], tag, self.text.strip())
-
-        elif tag =='item':
-            self.question_list[-1].items[-1].text = self.text.strip()
-
-        elif tag =='response':
-            self.question_list[-1].items[-1].response = self.text.strip()
-
-        elif tag == 'question':
-            # some error checking
-            if self.question_list[-1].type == None:
-                    webquiz_error('question {} does have not an \answer or multiple choice'.format(
-                                  len(self.question_list)+1))
-
-            elif hasattr(self.question_list[-1], 'items'):
-                if len(self.question_list[-1].items)==0:
-                    webquiz_error('question {} has no multiple choice items'.format(
-                                  len(self.question_list)+1))
-
-                if self.question_list[-1].type=='single' and self.question_list[-1].correct!=1:
-                    webquiz_error('question {} is single-choice but has {} correct answers'.format(
-                                    len(self.question_list)+1,
-                                    self.question_list[-1].correct
-                                 )
-                    )
-            elif not hasattr(self.question_list[-1], 'answer') or self.question_list[-1].answer=='':
-                webquiz_error('question {} does have not an \answer or multiple choice'.format(
-                              len(self.question_list)+1))
-
-            if self.text.strip() != '':
-                self.question_list[-1].after_text += ' '+self.text.strip()
 
         elif tag in ['breadcrumb', 'title', 'unit_code', 'unit_name']:
             setattr(self, tag, self.text.strip())
@@ -286,16 +291,73 @@ class QuizHandler(xml.sax.ContentHandler):
         elif tag.startswith('when'):
             setattr(self.question_list[-1], tag, self.text.strip())
 
-        elif tag == 'index_item':
-            # ungainly hack to remove line breaks from titles...
-            self.quiz_index[-1].title = self.text.strip().replace('\n',' ').replace('\r',' ')
-
         else:
-            # mark that we still need to use the text
-            text_used = False
+            # self.text lives to be used another day
+            reset_text = False
 
-        if text_used:
+        if reset_text:
             self.text = ''
+
+    #---- start of the end elements ------------------------------------------
+
+    def end_answer(self):
+        r'''
+        Process end tag when tag="answer"
+        '''
+        self.question_list[-1].answer = self.text.strip()
+
+    def end_discussion(self):
+        r'''
+        Process end tag when tag="discussion"
+        '''
+        self.discussion_list[-1].text = self.text.strip()
+
+    def end_item(self):
+        r'''
+        Process end tag when tag="item"
+        '''
+        self.question_list[-1].items[-1].text = self.text.strip()
+
+    def end_feedback(self):
+        r'''
+        Process end tag when tag="feedback"
+        '''
+        self.question_list[-1].items[-1].feedback = self.text.strip()
+
+    def end_question(self):
+        r'''
+        Process end tag when tag="question"
+        '''
+        # first some error checking
+        if self.question_list[-1].type == None:
+                webquiz_error('Question {} does not have an \\answer or choice environment'.format(
+                              len(self.question_list)+1))
+
+        elif hasattr(self.question_list[-1], 'items'):
+            if len(self.question_list[-1].items)==0:
+                webquiz_error('question {} has no multiple choice items'.format(
+                              len(self.question_list)+1))
+
+            if self.question_list[-1].type=='single' and self.question_list[-1].correct!=1:
+                webquiz_error('question {} is single-choice but has {} correct answers'.format(
+                                len(self.question_list)+1,
+                                self.question_list[-1].correct
+                             )
+                )
+        elif not hasattr(self.question_list[-1], 'answer') or self.question_list[-1].answer=='':
+            webquiz_error('question {} does have not an \answer or multiple choice'.format(
+                          len(self.question_list)+1))
+
+        if self.text.strip() != '':
+            self.question_list[-1].after_text += ' '+self.text.strip()
+
+    def end_index_item(self):
+        r'''
+        Process end tag when tag="index_item"
+        '''
+        self.quiz_index[-1].title = self.text.strip().replace('\n',' ').replace('\r',' ')
+
+    #---- end of end elements -----------------------------------------------
 
     def characters(self, text):  #data,start,length):
         r'''
